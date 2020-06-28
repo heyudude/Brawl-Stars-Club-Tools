608bf7ecfaa107b13306a21e49879b4104d8a56f
HeyuDude <102884+heyudude@users.noreply.github.com>
2020-06-28 11:04:51 +0200

    updates ok

--- bstools/__init__.py
+++ bstools/__init__.py
@@ -12,51 +12,53 @@ from .scorecalc import ScoreCalculator
 
 logger = logging.getLogger(__name__)
 
+
 def parse_args(argv):
     # parse command line arguments
-    parser = ArgumentParser(prog        = 'bstools',
-                            description = '''A tool for creating a dashboard for Club participation in
+    parser = ArgumentParser(prog='bstools',
+                            description='''A tool for creating a dashboard for Club participation in
                                              ClashRoyale. See https://developer.brawlstars.com to sign up
                                              for a developer account and create an API key to use with this.''')
     parser.add_argument('--locale',
-                        metavar  = 'LOCALE',
-                        help     = 'Locale if language other than English is desired'
+                        metavar='LOCALE',
+                        help='Locale if language other than English is desired'
                         )
     parser.add_argument('--config',
-                        metavar  = 'CONFIG-FILE',
-                        help     = 'configuration file for this app.')
+                        metavar='CONFIG-FILE',
+                        help='configuration file for this app.')
     parser.add_argument('--api_key',
-                        metavar  = 'KEY',
-                        help     = 'API key for developer.brawlstars.com')
-    parser.add_argument('--Club',
-                        metavar  = 'TAG',
-                        help    = 'Club ID from Brawl Stars. If it starts with a "#", Club ID must be quoted.')
-    parser.add_argument('--Player',
-                        metavar  = 'TAG',
-                        help    = 'Plaer ID from Brawl Stars. If it starts with a "#", Player ID must be quoted.')
+                        metavar='KEY',
+                        help='API key for developer.brawlstars.com')
+    parser.add_argument('--club',
+                        metavar='TAG',
+                        help='Club ID from Brawl Stars. If it starts with a "#", Club ID must be quoted.')
+    parser.add_argument('--player',
+                        metavar='TAG',
+                        help='Plaer ID from Brawl Stars. If it starts with a "#", Player ID must be quoted.')
     parser.add_argument('--out',
-                        metavar  = 'PATH',
-                        help     = 'Output path for HTML.')
+                        metavar='PATH',
+                        help='Output path for HTML.')
     parser.add_argument('--favicon',
-                        metavar  = 'PATH',
-                        help     = 'Source path for favicon.ico. If provided, we will copy to the output directory.')
+                        metavar='PATH',
+                        help='Source path for favicon.ico. If provided, we will copy to the output directory.')
     parser.add_argument('--club_logo',
-                        metavar  = 'PATH',
-                        help     = 'Source path for Club logo PNG. Recommended at least 64x64 pizels. If provided, we will copy to the output directory.')
+                        metavar='PATH',
+                        help='Source path for Club logo PNG. Recommended at least 64x64 pizels. If provided, we will copy to the output directory.')
     parser.add_argument('--description',
-                        metavar  = 'PATH',
-                        help     = 'Source path snippet of HTML to replace the Club description. Should not be a complete HTML document. Sample here: https://github.com/AaronTraas/bstools-agrassar-assets/blob/master/description.html\n\nIf provided, we will copy to the output directory.')
+                        metavar='PATH',
+                        help='Source path snippet of HTML to replace the Club description. Should not be a complete HTML document. Sample here: https://github.com/AaronTraas/bstools-agrassar-assets/blob/master/description.html\n\nIf provided, we will copy to the output directory.')
     parser.add_argument('--canonical_url',
-                        metavar  = 'URL',
-                        help     = 'Canonical URL for this site. Used for setting the rel=\'canonical\' link in the web site, as well as generating the robots.txt and sitemap.xml')
+                        metavar='URL',
+                        help='Canonical URL for this site. Used for setting the rel=\'canonical\' link in the web site, as well as generating the robots.txt and sitemap.xml')
     parser.add_argument('--debug',
-                        action   = 'store_true',
-                        help     = 'Turns on debug mode')
+                        action='store_true',
+                        help='Turns on debug mode')
     parser.add_argument('--version',
-                        action   = 'store_true',
-                        help     = 'List the version of bstools.')
+                        action='store_true',
+                        help='List the version of bstools.')
 
     return parser.parse_args(argv)
+
 
 def get_config_from_args(args, check_config_file=True, config_default='~/.bstools'):
     if args.version:
@@ -67,7 +69,8 @@ def get_config_from_args(args, check_config_file=True, config_default='~/.bstool
         config_file_name = args.config
         os.path.expanduser(config_file_name)
         if os.path.isfile(config_file_name) == False:
-            logger.error('Config file specified {} not found'.format(config_file_name) )
+            logger.error(
+                'Config file specified {} not found'.format(config_file_name))
             exit(1)
     else:
         config_file_name = os.path.expanduser(config_default)
@@ -81,10 +84,10 @@ def get_config_from_args(args, check_config_file=True, config_default='~/.bstool
     # grab API key and Plaer / Club ID from arguments if applicable
     if args.api_key:
         config['api']['api_key'] = args.api_key
-    if args.Club:
-        config['api']['club_id'] = args.Club
-    if args.Player:
-        config['api']['player_id'] = args.Player
+    if args.club:
+        config['api']['club_id'] = args.club
+    if args.player:
+        config['api']['player_id'] = args.player
     if args.out:
         config['paths']['out'] = args.out
     if args.favicon:
@@ -101,7 +104,8 @@ def get_config_from_args(args, check_config_file=True, config_default='~/.bstool
 
     return config
 
-def main(): # pragma: no cover
+
+def main():  # pragma: no cover
     args = parse_args(sys.argv[1:])
 
     config = get_config_from_args(args)
